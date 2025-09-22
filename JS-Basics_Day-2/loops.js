7d5cac7a0bf727b56e98105db8e91bddab165080
rajan-sc <rajanchauhan0608@gmail.com>
2025-09-22 11:30:22 +0530

    pc-

---
+++ JS-Basics_Day-2/loops.js
@@ -0,0 +1,24 @@
+
+
+
+// what is the difference between while and if statements?
+// if statement will run only once
+// while loop will run multiple times until the condition is false
+
+ 
+// while (i >= 1) {
+//     console.log(i);
+//     i-=1;
+// }  
+
+
+for (let j = 1; j <= 10; j++) {
+    console.log(j);
+}
+
+console.log("-------");
+j = 1;
+while (j <= 10) {
+    console.log(j);
+    j++;
+}
